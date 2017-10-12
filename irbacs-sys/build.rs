9a30c9459c31395482f3c2a6b1fa2531ed3158be
Pete Gadomski <pete.gadomski@gmail.com>
2017-10-12 13:46:45 +0000

    Reorganize into a workspace, add irbacs-sys
    Sys bindings for the irb sdk.

---
+++ irbacs-sys/build.rs
@@ -0,0 +1,16 @@
+extern crate bindgen;
+
+use bindgen::Builder;
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    println!("cargo:rustc-link-lib=irbacs_l64");
+    let bindings = Builder::default().header("wrapper.h").generate().expect(
+        "Unable to generate bindings",
+    );
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Unable to write bindings");
+}
