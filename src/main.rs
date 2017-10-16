ec396118647089a70292c7236cc7cac69a6933b0
Pete Gadomski <pete.gadomski@gmail.com>
2017-10-16 22:21:28 +0000

    Add camera name to info print

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ fn main() {
 
     if let Some(matches) = matches.subcommand_matches("info") {
         let irb = Irb::from_path(matches.value_of("FILE").unwrap()).expect("Could not open file");
+        println!("Camera name: {}", irb.camera_name().unwrap());
         println!("Image dimensions:");
         println!("  Width: {}", irb.image_width().unwrap());
         println!("  Height: {}", irb.image_height().unwrap());
