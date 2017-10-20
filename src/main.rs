5f37f022d28934415e1a7d1cfaff5ccb8d0c3584
Pete Gadomski <pete.gadomski@gmail.com>
2017-10-20 15:26:53 +0000

    Add pixel subcommand to main

--- src/main.rs
+++ src/main.rs
@@ -16,4 +16,14 @@ fn main() {
         println!("  Width: {}", irb.image_width().unwrap());
         println!("  Height: {}", irb.image_height().unwrap());
     }
+
+    if let Some(matches) = matches.subcommand_matches("pixel") {
+        let irb = Irb::from_path(matches.value_of("FILE").unwrap()).expect("Could not open file");
+        let u = value_t!(matches, "U", i32).unwrap_or_else(|e| e.exit());
+        let v = value_t!(matches, "V", i32).unwrap_or_else(|e| e.exit());
+        println!(
+            "{}",
+            irb.temperature(u, v).expect("Could not read pixel value") - 273.15
+        );
+    }
 }
